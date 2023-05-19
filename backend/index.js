4b3dbddb50970cf44ee4c32c5757988b6b748cdf
smithcabase <31966074+smithcabase@users.noreply.github.com>
2023-05-19 14:43:07 +0800

    uibuild

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const cors = require("cors");
+//const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const Note = require("./models/note");
+const { default: mongoose } = require("mongoose");
 
 let notes = [
   {
@@ -49,32 +50,29 @@ app.get("/api/notes/:id", (request, response) => {
 });
 
 app.delete("/api/notes/:id", (request, response) => {
-  const id = JSON.parse(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
-
-  response.status(204).end(`content with id ${id} deleted`);
+  Note.findById(request.params.id).then((note) => {
+    response.json(note);
+  });
 });
 
-const generateId = () => {
-  const maxId =
-    notes.length > 0 ? Math.max(...notes.map((note) => note.id)) : 0;
-  return maxId + 1;
-};
-
 app.post("/api/notes", (request, response) => {
+  console.log(request);
   const body = request.body;
 
-  if (!body.content) {
+  if (body.content === undefined) {
     return response.status(400).json({
       error: "content missing",
     });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  };
+  });
+
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 
   notes = notes.concat(note);
   //response.status(204);
